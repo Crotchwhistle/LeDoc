94abc66689b7bd7accaa0b9322d82eba89d442b1
Suhaan Sharma <subaanfeetpics@gmail.com>
2025-01-04 20:46:58 +1100

    editor tools

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -7,6 +7,7 @@ import Table from '@tiptap/extension-table'
 import TableCell from '@tiptap/extension-table-cell'
 import TableHeader from '@tiptap/extension-table-header'
 import TableRow from '@tiptap/extension-table-row'
+import Underline from '@tiptap/extension-underline'
 import Image from '@tiptap/extension-image'
 import ImageResize from 'tiptap-extension-resize-image'
 import { useEditor, EditorContent } from '@tiptap/react'
@@ -49,6 +50,7 @@ export const Editor = () => {
         },
         extensions: [
             StarterKit,
+            Underline,
             Image,
             ImageResize,
             Table,
