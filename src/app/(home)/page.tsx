b0fa9e7d9bdce2b40fec49e8641d3862c646e84c
Suhaan Sharma <subaanfeetpics@gmail.com>
2025-01-05 20:18:30 +1100

    template gallery on the home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
 
 import { Navbar } from "./navbar";
+import { TemplatesGallery } from "./templates-gallery";
 
 const Home = () => {
   return (
@@ -10,7 +11,7 @@ const Home = () => {
       </div>
 
       <div className="mt-16">
-        Click <Link href="/documents/123">here</Link> to go to document id
+        <TemplatesGallery />
       </div>
 
     </div>
